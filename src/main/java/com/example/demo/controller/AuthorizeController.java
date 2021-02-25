d964a3c970d7c5896ad1b27ac49c5c342c8adea3
wyhttxx <896082287@qq.com>
2021-02-25 08:15:21 +0800

    morning

--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -16,7 +16,16 @@ public class AuthorizeController {
     public String Callback(@RequestParam(name = "code") String code,
                            @RequestParam(name="state") String state
     ){
+//        commond alt v
+//        shift enter
         AcessTokenDTO acessTokenDTO = new AcessTokenDTO();
+        acessTokenDTO.setClient_id("5162d050da0daafd0f0a");
+        acessTokenDTO.setClient_secret("9619af3b58aa97ac1c1bd87a1f2f73851cc96208");
+        acessTokenDTO.setCode(code);
+        acessTokenDTO.setRedirect_uri("http:127.0.0.1:8887/callback");
+        acessTokenDTO.setState(state);
+
+
         githubProvider.getAcessToken(acessTokenDTO);
         return "index";
     }
--- src/main/java/com/example/demo/provider/GithubProvider.java
+++ src/main/java/com/example/demo/provider/GithubProvider.java
@@ -1,28 +1,42 @@
 package com.example.demo.provider;
+import com.alibaba.fastjson.JSON
 import com.example.demo.dto.AcessTokenDTO;
+import com.example.demo.dto.GithubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-
+//alt+enter引入jar包
 @Component
 public class GithubProvider {
     public String getAcessToken(AcessTokenDTO acessTokenDTO){
-        MediaType JSON
-                = MediaType.get("application/json; charset=utf-8");
+        MediaType mediaType
+                = MediaType.parse("application/json; charset=utf-8");
 
         OkHttpClient client = new OkHttpClient();
 
-        String post(String url, String json) throws IOException {
-            RequestBody body = RequestBody.create(JSON, json);
-            Request request = new Request.Builder()
-                    .url(url)
-                    .post(body)
-                    .build();
-            try (Response response = client.newCall(request).execute()) {
-                return response.body().string();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(acessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https:github.com/login/oauth/acess_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string= null;
+            try {
+                string = response.body().string();
+                System.out.println(string);
+                return string;
+//                alt+enter处理异常
+            } catch (IOException e) {
             }
+            return null
+
         }
+
+    }
+    public GithubUser getUser(String acessToken){
+
     }
 
 }
